c7c6fa86ec7f33e273a5f31e1ae14758c871721d
edesiri277 <edesiriavwarute@gmail.com>
2025-05-11 15:31:55 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Select the button by its ID
+const actionBtn = document.getElementById('actionBtn');
+
+// Add click event listener
+actionBtn.addEventListener('click', () => {
+  alert('Button was clicked!');
+});
